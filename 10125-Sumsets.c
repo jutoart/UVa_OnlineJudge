e74ff658133b436fdc8b2b04d6ecfdeb12e38cf5
jutoart <jutoart@gmail.com>
2015-06-19 21:32:26 +0800

    Add data type and code flow for input number sets

--- 10125-Sumsets.c
+++ 10125-Sumsets.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct NumSet_T {
+    int num;
+    int* pElements;
+} NumSet;
+
+typedef struct NumSetNode_T {
+    NumSet* pSet;
+    struct NumSetNode_T* pNextNode;
+} NumSetNode;
 
 void Swap (int* px, int* py) {
     int temp = *px;
@@ -34,5 +45,56 @@ void QuickSort (int* pNumArray, int arrayLength) {
 }
 
 int main () {
+    int num = 0;
+    NumSetNode* pInputSetList = NULL;
+    NumSetNode* pIndexSetNode = NULL;
+
+    scanf("%d", &num);
+
+    while (num > 0) {
+        int index = 0;
+        NumSetNode* pNewSetNode = (NumSetNode*)malloc(sizeof(NumSetNode));
+        pNewSetNode->pSet = (NumSet*)malloc(sizeof(NumSet));
+        pNewSetNode->pNextNode = NULL;
+        pNewSetNode->pSet->num = num;
+        pNewSetNode->pSet->pElements = (int*)malloc(num*sizeof(int));
+
+        for (index = 0; index < num; index++) {
+            scanf("%d", &(pNewSetNode->pSet->pElements[index]));
+        }
+
+        if (pInputSetList == NULL) {
+            pInputSetList = pNewSetNode;
+        }
+        else {
+            pIndexSetNode->pNextNode = pNewSetNode;
+        }
+
+        pIndexSetNode = pNewSetNode;
+        scanf("%d", &num);
+    }
+
+    pIndexSetNode = pInputSetList;
+
+    while (pIndexSetNode != NULL) {
+        int index = 0;
+
+        printf("%d\n", pIndexSetNode->pSet->num);
+
+        for (index = 0; index < pIndexSetNode->pSet->num; index++) {
+            printf("%d ", pIndexSetNode->pSet->pElements[index]);
+        }
+
+        printf("\n");
+        pIndexSetNode = pIndexSetNode->pNextNode;
+    }
+
+    while (pIndexSetNode != NULL) {
+        pIndexSetNode = pInputSetList;
+        pInputSetList = pIndexSetNode->pNextNode;
+        free(pIndexSetNode->pSet);
+        free(pIndexSetNode);
+    }
+
     return 0;
 }
