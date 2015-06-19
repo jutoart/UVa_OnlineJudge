4fe90e675753aab5c7f50416c33f656fabd4506a
jutoart <jutoart@gmail.com>
2015-06-19 18:01:26 +0800

    Modified problem 100, the 3n+1 problem, to dynamic programming version

--- 100-The_3n+1_problem.c
+++ 100-The_3n+1_problem.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_NUM 1000000
+
 typedef struct NumSet_T {
     int firstNum;
     int secondNum;
@@ -11,6 +13,11 @@ typedef struct LinkedListNode_T {
     struct LinkedListNode_T* pNextNode;
 } LinkedListNode;
 
+typedef struct NumListNode_T {
+    int number;
+    struct NumListNode_T* pNextNode;
+} NumListNode;
+
 void AppendLinkedListNode (LinkedListNode** ppLastNode, LinkedListNode* pNewNode) {
     (*ppLastNode)->pNextNode = pNewNode;
     *ppLastNode = pNewNode;
@@ -20,6 +27,9 @@ void AppendLinkedListNode (LinkedListNode** ppLastNode, LinkedListNode* pNewNode
 int main () {
     LinkedListNode* pStartNode = NULL;
     LinkedListNode* pIndexNode = NULL;
+    int countBuf[MAX_NUM] = {0};
+
+    countBuf[0] = 1;
 
     while (1) {
         int inputFirstNum = 0, inputSecondNum = 0;
@@ -60,22 +70,46 @@ int main () {
         }
 
         for (index = startNum; index <= endNum; index++) {
-            int count = 1;
             int num = index;
+            NumListNode* pStartNumNode = NULL;
 
-            while (num > 1) {
-                if (num % 2 == 0) {
-                    num = num >> 1;
-                    count++;
+            while (num > 0) {
+                if ((num < MAX_NUM) && (countBuf[num-1] > 0)) {
+                    int count = countBuf[num-1];
+
+                    while(pStartNumNode != NULL) {
+                        NumListNode* pCurNumNode = pStartNumNode;
+
+                        count++;
+
+                        if (pStartNumNode->number < MAX_NUM) {
+                            countBuf[pStartNumNode->number-1] = count;
+                        }
+
+                        pStartNumNode = pStartNumNode->pNextNode;
+                        free(pCurNumNode);
+                    }
+
+                    break;
                 }
                 else {
-                    num = ((num + 1) >> 1) + num;
-                    count += 2;
+                    NumListNode* pNewNumNode = (NumListNode*)malloc(sizeof(NumListNode));
+
+                    pNewNumNode->number = num;
+                    pNewNumNode->pNextNode = pStartNumNode;
+                    pStartNumNode = pNewNumNode;
+
+                    if (num % 2 == 0) {
+                        num = num >> 1;
+                    }
+                    else {
+                        num = num * 3 + 1;
+                    }
                 }
             }
 
-            if (maxCount < count) {
-                maxCount = count;
+            if (maxCount < countBuf[index-1]) {
+                maxCount = countBuf[index-1];
             }
         }
 
