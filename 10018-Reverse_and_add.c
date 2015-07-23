13eab1bb3eef3877bbe0d67acdecc80d6a7d4b68
jutoart <jutoart@gmail.com>
2015-07-24 00:56:34 +0800

    Fix digit reversing function. Third submission version.

--- 10018-Reverse_and_add.c
+++ 10018-Reverse_and_add.c
@@ -6,38 +6,24 @@
 
 unsigned int ReverseDigits (unsigned int num) {
     int count = 0;
-    unsigned int max = 0;
-    unsigned int cur10Times = 10, pre10Times = 0;
     int index = 0;
     unsigned int result = 0;
     int buffer[MAX_DIGIT_SIZE] = {0};
 
-    while (num > max) {
-        max = max * 10 + 9;
-
-        if (cur10Times == 10) {
-            buffer[count] = num % cur10Times;
-        }
-        else {
-            buffer[count] = ((num % cur10Times) - (num % pre10Times)) / pre10Times;
-        }
-
-        pre10Times = cur10Times;
-        cur10Times *= 10;
+    while (num > 0) {
+        buffer[count] = num % 10;
+        num /= 10;
         count++;
     }
 
-    cur10Times = 1;
-
-    for (index = count-1; index >= 0; index--) {
-        result += buffer[index] * cur10Times;
-        cur10Times *= 10;
+    for (index = 0; index < count; index++) {
+        result = result * 10 + buffer[index];
     }
 
     return result;
 }
 
-int FindPalindrome (int num, unsigned int* pPalindrome, int* pIterCountData, unsigned int* pPalindromeData) {
+int FindPalindrome (unsigned int num, unsigned int* pPalindrome, int* pIterCountData, unsigned int* pPalindromeData) {
     if (num < MAX_DATABASE_SIZE) {
         if (pIterCountData[num] > 0) {
             *pPalindrome = pPalindromeData[num];
@@ -47,13 +33,7 @@ int FindPalindrome (int num, unsigned int* pPalindrome, int* pIterCountData, uns
 
     if (num == ReverseDigits(num)) {
         *pPalindrome = num;
-
-        if (num < MAX_DATABASE_SIZE) {
-            pIterCountData[num] = 0;
-            pPalindromeData[num] = num;
-        }
-
-        return 0;
+        return 1;
     }
     else {
         unsigned int reverseNum = ReverseDigits(num);
@@ -90,8 +70,10 @@ int main () {
     }
 
     for (index = 0; index < inputNum; index++) {
+        unsigned int reverseNum = ReverseDigits(input[index]);
+        unsigned int sum = input[index] + reverseNum;
         unsigned int palindrome = 0;
-        int count = FindPalindrome(input[index], &palindrome, iterCountDataBase, palindromeDataBase);
+        int count = FindPalindrome(sum, &palindrome, iterCountDataBase, palindromeDataBase);
 
         printf("%d %u\n", count, palindrome);
     }
