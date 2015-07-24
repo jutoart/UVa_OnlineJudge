dd48c8daf146800dd74b06dc45d35d5d8a2ae9d8
jutoart <jutoart@gmail.com>
2015-07-25 00:43:44 +0800

    Add the consideration for only greater number can subtract the smaller one. Second submission version.

--- 10055-Hashmat_the_brave_warrior.c
+++ 10055-Hashmat_the_brave_warrior.c
@@ -4,7 +4,12 @@ int main () {
     unsigned int num1 = 0, num2 = 0;
 
     while (scanf("%u %u", &num1, &num2) == 2) {
-        printf("%u\n", num2 - num1);
+        if (num1 >= num2) {
+            printf("%u\n", num1 - num2);
+        }
+        else {
+            printf("%u\n", num2 - num1);
+        }
     }
 
     return 0;
